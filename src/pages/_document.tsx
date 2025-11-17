03463ef137cb4c56216062886641714e67b86425
Harold Jimenez <harold3112@gmail.com>
2025-11-17 15:03:54 -0400

    Preload para los videos

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Preload de videos para acelerar el primer hover */}
+        <link rel="preload" as="video" href="/videos/Swatex-vid.mov" type="video/quicktime" />
+        <link rel="preload" as="video" href="/videos/grupochavon-vid.mov" type="video/quicktime" />
+        <link rel="preload" as="video" href="/videos/Lromana-vid.mov" type="video/quicktime" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
